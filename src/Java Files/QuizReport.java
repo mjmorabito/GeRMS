ce2918100867e6120383bbb5a2431fd428ad9c17
Samir <Samir@10.10.252.242>
2015-04-23 19:25:51 -0400

    removed jasper

--- src/Java Files/QuizReport.java
+++ src/Java Files/QuizReport.java
@@ -14,12 +14,6 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.ImageIcon;
 import javax.swing.table.DefaultTableModel;
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperCompileManager;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.data.JRTableModelDataSource;
-import net.sf.jasperreports.view.JasperViewer;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -324,46 +318,9 @@ public class QuizReport extends javax.swing.JInternalFrame {
     }//GEN-LAST:event_helpAudioButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-        
-        // Run jasper report
-        JasperPrint jasperPrint = null;
-        
-        TableModelData();
-        
-        try {
-            JasperCompileManager.compileReportToFile("src/JasperReports/ParameterExample.jrxml");
-            jasperPrint = JasperFillManager.fillReport("src/JasperReports/ParameterExample.jasper", new HashMap(),
-                    new JRTableModelDataSource(tableModel));
-            JasperViewer jasperViewer = new JasperViewer(jasperPrint);
-            // jasperViewer.setVisible(true);
-            JasperViewer.viewReport(jasperPrint, false);
-            //jasperViewer.setDefaultCloseOperation(.DISPOSE_ON_CLOSE);
-        } catch (JRException ex) {
-            ex.printStackTrace();
-        }
-        
+
     }//GEN-LAST:event_jButton2ActionPerformed
 
-    
-    public void TableModelData() {
-        
-        String[] columnNames = {"Id", "Name", "Department", "Email"};
-        String[][] data = {
-            {"111", "G Conger", " Orthopaedic", "jim@wheremail.com"},
-            {"222", "A Date", "ENT", "adate@somemail.com"},
-            {"333", "R Linz", "Paedriatics", "rlinz@heremail.com"},
-            {"444", "V Sethi", "Nephrology", "vsethi@whomail.com"},
-            {"555", "K Rao", "Orthopaedics", "krao@whatmail.com"},
-            {"666", "V Santana", "Nephrology", "vsan@whenmail.com"},
-            {"777", "J Pollock", "Nephrology", "jpol@domail.com"},
-            {"888", "H David", "Nephrology", "hdavid@donemail.com"},
-            {"999", "P Patel", "Nephrology", "ppatel@gomail.com"},
-            {"101", "C Comer", "Nephrology", "ccomer@whymail.com"}
-        };
-        tableModel = new DefaultTableModel(data, columnNames);
-    }
-       
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton helpAudioButton1;
