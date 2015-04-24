1cbdc69b0358b5a5a0e71ffaf7d4e1c7a8c5f27a
Samir <Samir@10.0.0.7>
2015-04-24 19:41:21 -0400

    PrintReport screen

--- src/JavaFiles/PrintReports.java
+++ src/JavaFiles/PrintReports.java
@@ -262,7 +262,7 @@ public class PrintReports extends javax.swing.JInternalFrame {
                 java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class, java.lang.String.class
             };
             boolean[] canEdit = new boolean [] {
-                false, false, true, false, false
+                false, false, false, false, false
             };
 
             public Class getColumnClass(int columnIndex) {
