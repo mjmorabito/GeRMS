70b1a521d39fe373bba3e834950418db2eea0400
mjmorabito <mjmorabito@gmail.com>
2015-04-24 12:51:02 -0400

    Titles for Assessments are good.

--- src/Java Files/KN1.java
+++ src/Java Files/KN1.java
@@ -6,12 +6,8 @@
  */
 
 import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.sound.sampled.AudioFormat;
